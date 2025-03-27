9934e0ddf9defd39435ae8a1994caba487adf63b
Suresh Shrestha <sureshcstha@gmail.com>
2025-03-27 11:44:52 -0600

    Check if the long URL already exists in the database. If exists, return the same short URL instead of creating a new short URL

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ aws cloudformation deploy --template-file infrastructure/dynamoDB-setup.yaml --s
 ```
 serverless deploy
 ```
+
+### How to delete the failed stack before re-deploying?
+*Use this command with caution, as it will permanently delete the resources associated with the stack.
+```
+aws cloudformation delete-stack --stack-name url-shortener-db
+
+```
--- lambdas/createShortUrl.js
+++ lambdas/createShortUrl.js
@@ -5,19 +5,41 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
   const { originalUrl } = JSON.parse(event.body);
-  const shortCode = uuidv4().slice(0, 7); // Generate a 7-char short code
 
-  const params = {
+  // First, check if the URL already exists in the database
+  const checkParams = {
     TableName: 'URLMappings',
-    Item: {
-        shortCode,
-        originalUrl,
-        clicks: 0
+    IndexName: 'originalUrl-index',
+    KeyConditionExpression: 'originalUrl = :originalUrl',
+    ExpressionAttributeValues: {
+      ':originalUrl': originalUrl,
     },
   };
 
   try {
-    await dynamoDB.put(params).promise();
+    const checkResult = await dynamoDB.query(checkParams).promise();
+    let shortCode;
+
+    if (checkResult.Items.length > 0) {
+      // URL already exists, so use the existing short code
+      shortCode = checkResult.Items[0].shortCode;
+    } else {
+      // URL doesn't exist, generate a new short code
+      shortCode = uuidv4().slice(0, 7); // Generate a 7-char short code
+
+      // Save the new short code to DynamoDB
+      const params = {
+        TableName: 'URLMappings',
+        Item: {
+          shortCode,
+          originalUrl,
+          clicks: 0,
+        },
+      };
+
+      await dynamoDB.put(params).promise();
+    }
+
     return {
       statusCode: 201,
       body: JSON.stringify({ shortCode, shortUrl: `https://shresthatech.com/${shortCode}` }),
